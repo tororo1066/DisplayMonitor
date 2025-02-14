a5ca992524f67df503185aa1b88f912eaf333a73
tororo_1066 <panshoku4@gmail.com>
2025-02-14 13:47:27 +0900

    Docs Mechanism2

---
+++ GenDocs/build.gradle.kts
@@ -0,0 +1,42 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
+}
+
+val pluginVersion: String by project.ext
+
+group = "tororo1066"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(rootProject)
+    implementation("io.papermc.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT")
+    implementation("com.google.code.gson:gson:2.12.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+}
+
+tasks.named("build") {
+    dependsOn("shadowJar")
+}
+
+//メインクラスの指定
+application {
+    mainClass.set("tororo1066.displaymonitor.GenerateDocData")
+}
