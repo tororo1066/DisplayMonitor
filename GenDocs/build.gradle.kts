9defb38f8676cdab3d59394c5e1f82197ad44136
tororo_1066 <panshoku4@gmail.com>
2025-12-07 20:49:04 +0900

    GenDocsにもTororoPluginAPIの依存関係を追加

--- GenDocs/build.gradle.kts
+++ GenDocs/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 }
 
 val pluginVersion: String by project.ext
+val apiVersion: String by project.ext
 
 group = "tororo1066"
 
@@ -20,6 +21,7 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation(rootProject)
     implementation(project(":DisplayMonitorAPI"))
+    implementation("tororo1066:tororopluginapi:$apiVersion")
     implementation("io.papermc.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT")
     implementation("com.google.code.gson:gson:2.12.1")
 
