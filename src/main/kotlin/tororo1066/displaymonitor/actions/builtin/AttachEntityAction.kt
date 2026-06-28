632b4e1a1c1a21d58512cdf446747c058a71ae51
tororo_1066 <panshoku4@gmail.com>
2026-06-29 01:42:49 +0900

    Add documentation annotations and rename element variable to name in AttachEntityAction

--- src/main/kotlin/tororo1066/displaymonitor/actions/builtin/AttachEntityAction.kt
+++ src/main/kotlin/tororo1066/displaymonitor/actions/builtin/AttachEntityAction.kt
@@ -1,18 +1,34 @@
 package tororo1066.displaymonitor.actions.builtin
 
 import tororo1066.displaymonitor.actions.SuspendAction
+import tororo1066.displaymonitor.documentation.ClassDoc
+import tororo1066.displaymonitor.documentation.ParameterDoc
 import tororo1066.displaymonitorapi.actions.ActionResult
 import tororo1066.displaymonitorapi.actions.IActionContext
 import tororo1066.displaymonitorapi.configuration.IAdvancedConfigurationSection
 
+@ClassDoc(
+    name = "AttachEntity",
+    description = "targetに対して指定したElementをアタッチする。"
+)
 class AttachEntityAction: SuspendAction() {
 
-    var element: String = ""
+    @ParameterDoc(
+        name = "name",
+        description = "アタッチするElementの名前。",
+        default = ""
+    )
+    var name: String = ""
+    @ParameterDoc(
+        name = "forceSync",
+        description = "強制的に同期的に実行するか。",
+        default = "false"
+    )
     var forceSync: Boolean = false
 
     override suspend fun runSuspend(context: IActionContext): ActionResult {
         val target = context.target ?: return ActionResult.targetRequired()
-        val element = context.publicContext.elements[element] ?: return ActionResult.noParameters("Element not found: $element")
+        val element = context.publicContext.elements[name] ?: return ActionResult.noParameters("Element not found: $name")
         forceSync.orBlockingTask {
             element.attachEntity(target)
         }
@@ -20,7 +36,7 @@ class AttachEntityAction: SuspendAction() {
     }
 
     override fun prepare(configuration: IAdvancedConfigurationSection) {
-        element = configuration.getString("element", element) ?: element
+        name = configuration.getString("name", name) ?: name
         forceSync = configuration.getBoolean("forceSync", forceSync)
     }
 }
