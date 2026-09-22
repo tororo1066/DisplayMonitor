8899226d95d3611d7fbdf541d0ae2d59ffcbe985
tororo_1066 <panshoku4@gmail.com>
2026-09-23 03:24:54 +0900

    Add `apply` parameter to EditElement for conditional application of edits

--- src/main/kotlin/tororo1066/displaymonitor/actions/builtin/EditElement.kt
+++ src/main/kotlin/tororo1066/displaymonitor/actions/builtin/EditElement.kt
@@ -2,7 +2,6 @@ package tororo1066.displaymonitor.actions.builtin
 
 import tororo1066.displaymonitor.DisplayMonitor
 import tororo1066.displaymonitor.actions.SuspendAction
-import tororo1066.displaymonitor.configuration.AdvancedConfiguration
 import tororo1066.displaymonitor.documentation.ClassDoc
 import tororo1066.displaymonitor.documentation.ParameterDoc
 import tororo1066.displaymonitorapi.actions.ActionResult
@@ -26,15 +25,33 @@ class EditElement: SuspendAction() {
     )
     var edit: IAdvancedConfigurationSection? = null
     @ParameterDoc(
+        name = "apply",
+        description = "編集内容を適用するか。",
+        default = "true"
+    )
+    var apply = true
+    @ParameterDoc(
         name = "forceSync",
-        description = "強制的に同期的に実行するか。"
+        description = "強制的に同期的に実行するか。",
+        default = "false"
     )
     var forceSync = false
 
     override suspend fun runSuspend(context: IActionContext): ActionResult {
         val element = context.publicContext.elements[name] ?: return ActionResult.noParameters(DisplayMonitor.translate("action.editElement.notFound", name))
+        val edit = edit
         forceSync.orBlockingTask {
-            element.edit(edit ?: AdvancedConfiguration())
+            if (apply) {
+                if (edit == null) {
+                    element.applyChanges()
+                } else {
+                    element.edit(edit)
+                }
+            } else {
+                if (edit != null) {
+                    element.prepare(edit)
+                }
+            }
         }
         return ActionResult.success()
     }
@@ -42,6 +59,7 @@ class EditElement: SuspendAction() {
     override fun prepare(section: IAdvancedConfigurationSection) {
         name = section.getString("name", "")!!
         edit = section.getAdvancedConfigurationSection("edit")
+        apply = section.getBoolean("apply", true)
         forceSync = section.getBoolean("forceSync", false)
     }
 }
